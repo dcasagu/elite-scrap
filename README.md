4ad4b5c1bbb0c2fe6e0317aba81c1489be66e1ff
theroich <theroich@gmail.com>
2017-03-16 01:30:13 +0100

    init

---
+++ README.md
@@ -0,0 +1,50 @@
+# ELITE-SCRAPER
+Scraper de EliteTorrent
+## Buscar por secciones
+```javascript
+var elite = require('elite-scrap');
+elite.findHd().then(function(resultados){
+    console.log(resultados)
+});
+
+/*
+[ { title: 'Un traidor como los nuestros (HDRip)',
+    image: 'http://www.elitetorrent.net/thumb_fichas/35865_g.jpg',
+    desc: 'Una joven pareja británica se va de vacaciones a Marruecos. Allí conocen a un carismático millonario ruso que asegura pertenecer a la mafia rusa, donde es el mejor del mundo blanqueando dinero. El mafioso les invita a una fiesta donde les pide ayuda para solicitar asilo político en Inglaterra a cambio de contar todo lo que sabe, desenmascarando a todos los implicados, sus compañeros mafiosos, banqueros e incluso políticos británicos... Adaptación de la novela homónima de John le Carré.',
+    magnet: 'magnet:?xt=urn:btih:7BRCDR53V7QOZAD3QPOMMMKG5M3KUX5E&dn=Un+traidor+como+los+nuestros+%28HDRip%29+%28EliteTorrent.net%29&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce' },
+  { title: 'Marea negra (HDRip)',
+    image: 'http://www.elitetorrent.net/thumb_fichas/35863_g.jpg',
+    desc: 'Basado en los eventos sucedidos en el Golfo de México en abril del 2010, cuando un accidente en un oleoducto causó una catástrofe que mató a 11 personas e hirió a otras 16, provocando además una de las catástrofes medioambientales más graves causadas por el hombre.',
+    magnet: 'magnet:?xt=urn:btih:FOXCURVKORORR7TTM6W7IQGV43YMX3KU&dn=Marea+negra+%28HDRip%29+%28EliteTorrent.net%29&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce' },
+  .
+  .
+  .]
+*/
+```
+### Metodos
+- HD: ''findHd()''
+- MicroHD : ''findMicroHd()''
+- Estrenos : ''findEstrenos()''
+- Documentales y tv : ''findDocTv()''
+
+
+## Busqueda por cadena de texto
+```javascript
+var elite = require('elite-scrap');
+elite.findBySearch('Juego de tronos').then(function(resultados){
+    console.log(resultados);
+});
+```
+
+### Paginacion de los resultados
+```javascript
+var elite = require('elite-scrap');
+/*Parámetro 2 es el numero de pagina (en EliteTorrent de los que se muestran resultados*/
+elite.findBySearch('Juego de tronos',2).then(function(resultados){
+    console.log(resultados);
+});
+
+elite.findMicroHd(2).then(function(resultados){
+    console.log(resultados);
+});
+```
---
+++ app.js
@@ -0,0 +1,101 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var _ = require('lodash');
+var domain = 'http://www.elitetorrent.net';
+var async = require('async');
+var Q = require('q');
+var options = {
+    url: domain+'/categoria/17/peliculas-microhd'
+};
+var options = {
+
+    microhd : {
+        url:domain+'/categoria/17/peliculas-microhd'
+    },
+    hd : {
+        url:domain+'/categoria/13/peliculas-hdrip'
+    },
+    estrenos: {
+        url:domain+'/categoria/1/estrenos'
+    },
+    doctv: {
+        url:domain+'categoria/6/docus-y-tv'
+    },
+    search :{
+        url : domain+'/resultados/{str}'
+    }
+};
+
+exports.findHd = function(page){
+    return findTorrents(options.hd,page);
+};
+
+exports.findMicroHd = function(page){
+    return findTorrents(options.microhd,page);
+};
+
+exports.findEstrenos = function(page){
+    return findTorrents(options.estrenos,page);
+};
+
+exports.findDocTv = function(page){
+    return findTorrents(options.doctv,page);
+};
+
+exports.findBySearch = function(str,page){
+    var optionForSearch ={url: options.search.url.replace('{str}',str)};
+    return findTorrents(optionForSearch,page);
+};
+
+
+function findTorrents(option,page){
+
+    if(page) page = '/pag:'+page;
+    else page = '';
+    console.log(option);
+    var optionWithPage = {url: option.url+page};
+    var deferred = Q.defer();
+    request(optionWithPage, function (err, resp, html) {
+
+        if (err) deferred.reject(err);
+        var $ = cheerio.load(html);
+        var boxes = $('.miniboxs.miniboxs-ficha li');
+        var links = _(boxes).map(function(box){return $(box).find('a')})
+            .map(function(link){return $(link).attr('href')})
+            .map(function(tLink){return {url: domain+tLink}}).value();
+
+
+        function iterateFun(link,callback) {
+            request(link, function (err, resp, html) {
+
+                callback(null, html);
+            });
+        }
+
+        async.map(links,iterateFun,function(err,res){
+            if(err) deferred.reject(err);
+            var results = _(res).map(function(html){
+                return cheerio.load(html);
+            }).map(function($){
+                return {
+                    title : $('#box-ficha > h2').html(),
+                    image: domain+'/'+$('#box-ficha > .secc-izq > img').attr('src'),
+                    desc: (_($('#box-ficha > .secc-ppal > .descrip').text().split('\n')).last().split(':')[1]) ? (_($('#box-ficha > .secc-ppal > .descrip').text().split('\n')).last().split(':')[1]).trim() : '',
+                    magnet: $($('.enlace_torrent')[1]).attr('href')
+                }
+            }).value();
+
+            deferred.resolve(results);
+        });
+
+
+    });
+
+    return deferred.promise;
+
+}
+
+
+exports.findHd().then(function(res){
+    console.log(res);
+});
